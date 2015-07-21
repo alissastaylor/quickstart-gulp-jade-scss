f8562b78cb2873cd3fd979e7a66fec64f74c8954
Brandon Petty <bpetty@hirezstudios.com>
2015-07-21 12:11:34 -0400

    add: new gulp workflow and plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,58 @@
+// Requirements
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var open = require('gulp-open');
 var glue = require('gulp-glue');
+var size = require('gulp-size');
+var imageMin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var comOpen = require('open');
+var comDel = require('del');
 
-// Watch
-
-gulp.task('watch', function() {
-  gulp.watch('./_dev/scss/*.scss', ['sass']);
-  gulp.watch('./_dev/*.jade', ['jade']);
-  gulp.watch('./_dev/js/app.js', ['javascript']);
+// Optimize images
+gulp.task('images', function () {
+  return gulp.src('./_dev/img/*')
+    .pipe(plugins.cache(plugins.imagemin({ progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./_prod/img/'))
+    .pipe(size({ title: 'images' }));
 });
 
 // SCSS
-
 gulp.task('sass', function() {
-  gulp.src('./_dev/scss/*.scss')
+  gulp.src('./_dev/scss/**/*.scss')
   .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(autoprefixer({ browsers: [ 'last 2 versions' ] }))
   .pipe(gulp.dest('./_prod/css/'));
-});
-
-// Compile Jade
-
-gulp.task('jade', function() {
-  gulp.src('./_dev/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('./_prod/'))
+  .pipe(size({ title: 'stylesheets' }));
 });
 
 // Minify JS
-
 gulp.task('javascript', function() {
   return gulp.src('./_dev/js/scripts.js')
     .pipe(uglify())
     .pipe(gulp.dest('./_prod/js/'));
+    .pipe(size({ title: 'scripts' }));
 });
 
-// Moves Images
+// Compile Jade
+gulp.task('jade', function() {
+  gulp.src('./_dev/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./_prod/'))
+    .pipe(size({ title: 'html' }));
+});
 
-gulp.task('images', function() {
-  return gulp.src('./_dev/img/*')
-    .pipe(gulp.dest('./_prod/img/'))
+// Watch
+gulp.task('watch', [ 'javascript', 'sass', 'jade' ], function() {
+  browserSync({ notify: false, server: ['.tmp', 'app'] });
+  gulp.watch(['./_dev/*.jade'], browserSync.reload);
+  gulp.watch(['./_dev/scss/**/*.scss'], ['styles', browserSync.reload]);
+  gulp.watch(['./_dev/js/app.js'], ['scripts', browserSync.reload]);
 });
 
 gulp.task('default', ['sass','jade','javascript','images']);
